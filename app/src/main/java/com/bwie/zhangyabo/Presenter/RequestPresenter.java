695cb69b954638cd1d5c0340f729fa3a78f1fd2e
Moyuchen <31385093+Moyuchen@users.noreply.github.com>
2017-11-07 21:19:13 +0800

    lastest

--- app/src/main/java/com/bwie/zhangyabo/Presenter/RequestPresenter.java
+++ app/src/main/java/com/bwie/zhangyabo/Presenter/RequestPresenter.java
@@ -4,6 +4,8 @@ import com.bwie.zhangyabo.Bean.bean;
 import com.bwie.zhangyabo.Modle.RequestModle;
 import com.bwie.zhangyabo.Modle.RequestModleView;
 
+import java.lang.ref.WeakReference;
+
 import retrofit2.Call;
 import retrofit2.Response;
 
@@ -15,9 +17,11 @@ import retrofit2.Response;
 public class RequestPresenter implements RequestModleView {
     private RequestPresenterView requestPresenterView;
     private final RequestModle requestModle;
+    private WeakReference<RequestPresenterView> weakReference;
 
     public RequestPresenter(RequestPresenterView requestPresenterView) {
         this.requestPresenterView = requestPresenterView;
+        weakReference=new WeakReference<RequestPresenterView>(requestPresenterView);
         requestModle = new RequestModle();
         requestModle.setRequestModleView(this);
     }
@@ -29,12 +33,17 @@ public class RequestPresenter implements RequestModleView {
 
     @Override
     public void onFailue(Call call) {
-        requestPresenterView.onFailue(call);
+//        requestPresenterView.onFailue(call);
+        weakReference.get().onFailue(call);
     }
 
     @Override
     public void onResponse(Call call, Response<bean> response) {
-        requestPresenterView.onResponse(call,response);
+        weakReference.get().onResponse(call,response);
+    }
+
+    public void Disattach(){
+        weakReference.clear();
     }
 
 
